45fe064569d9960d501a38baf387cbc7eaea2c49
Jarrod Bogard <jarrodbogard@gmail.com>
2024-08-30 16:57:30 -0400

    change text

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -4,7 +4,7 @@ export default function Nav() {
       <span>Notifications</span>
       <span>Alerts</span>
       <span>|</span>
-      <span>username/photo</span>
+      <span>user/photo</span>
     </nav>
   );
 }
